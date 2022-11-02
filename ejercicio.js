9717c604fa9cea28af83fb9a8911160ab28557b2
DavidQ2001 <davidestebanq65@gmail.com>
2022-11-01 20:31:34 -0500

    ejercicios

---
+++ ejercicio.js
@@ -0,0 +1,24 @@
+
+ 
+let mascota = prompt("escriba un animal")
+
+mascota = 'lagarto'
+
+switch ( mascota ) {
+    case "lagarto":
+     alert ( "tengo una mascota" ) ;
+    break ;
+    case "elefante" :
+    alert ( "tengo una mascota" ) ;
+    break ;
+    case "gato" :
+    alert ( " tengo una mascota " ) ;
+    break ;
+
+    default:
+        alert ("no tengo esa mascota")
+        break;
+    
+ 
+
+}
